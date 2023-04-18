0efefcfe140e3714aa39eb61d23cb1614dc8d96b
Ubuntu <ubuntu@ip-172-31-34-183.ap-northeast-2.compute.internal>
2023-04-18 19:58:20 +0000

    Part II: Dealing with multiple concurrent requests

---
+++ .proxy/tiny.c
@@ -0,0 +1,268 @@
+/* $begin tinymain */
+/*
+ * tiny.c - A simple, iterative HTTP/1.0 Web server that uses the
+ *     GET method to serve static and dynamic content.
+ *
+ * Updated 11/2019 droh
+ *   - Fixed sprintf() aliasing issue in serve_static(), and clienterror().
+ */
+#include "csapp.h"
+
+// sprintf(): 지정된 문자열 버퍼에 서식화된 문자열을 써넣는다.
+// fprintf(): 파일(또는 다른 출력 스트림)에 서식화된 문자열을 써넣는다. 파일 뿐만 아니라 다른 출력 스트림(예: 표준 출력, 표준 에러 등)에도 쓸 수 있다.
+
+void doit(int fd);
+void read_requesthdrs(rio_t *rp);
+int parse_uri(char *uri, char *filename, char *cgiargs);
+void serve_static(int fd, char *filename, int filesize, char *method);
+void get_filetype(char *filename, char *filetype);
+void serve_dynamic(int fd, char *filename, char *cgiargs, char *method);
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
+
+// 입력이 ./tiny 8000 일 때, argc==2, argv[0]=="tiny", argv[1]==8000 이다. 
+// 리눅스 시스템 프로그래밍에서 int main의 매개변수는 argc – 명령행 인자 개수, argv – 명령행 인자 벡터 (문자열의 배열), envp – 환경 변수 목록.
+int main(int argc, char **argv) {
+    int listenfd, connfd;
+    char hostname[MAXLINE], port[MAXLINE];
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+
+    /* Check command-line args */
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    /* Open_listenfd 함수를 호출해서 듣기 소켓을 오픈한다. 인자로 포트번호를 넘겨준다. */
+    // Open_listenfd는 요청받을 준비가 된 듣기 식별자를 리턴한다. <-- listenfd
+    listenfd = Open_listenfd(argv[1]);
+
+    /* 전형적인 무한 서버 루프를 실행 */
+    while (1) {
+		// accept 함수 인자에 넣기 위한 주소 길이를 계산한다.
+		clientlen = sizeof(clientaddr);
+
+		/* 반복적으로 연결 요청을 접수 */
+		// accept 함수는 1. 듣기 식별자, 2. 소켓주소구조체의 주소, 3. 주소(소켓구조체)의 길이를 인자로 받는다.
+		connfd = Accept(listenfd, (SA *) &clientaddr, &clientlen);
+
+		// Getaddrinfo는 호스트 이름: 호스트 주소, 서비스 이름: 포트 번호의 스트링 표시를 소켓 주소 구조체로 변환.
+		// Getnameinfo는 위를 반대로 소켓 주소 구조체에서 스트링 표시로 변환.
+		Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+		printf("Accepted connection from (%s, %s)\n", hostname, port);
+
+		/* 트랜젝션을 수행 */
+		doit(connfd);
+
+		/* 트랜잭션이 수행된 후 자신 쪽의 연결 끝 (소켓) 을 닫는다. */
+		Close(connfd); // 자신 쪽의 연결 끝을 닫는다.
+        printf("===============================================\n\n");
+    }
+}
+
+void doit(int fd) {
+	int is_static;
+	struct stat sbuf;
+	char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+	char filename[MAXLINE], cgiargs[MAXLINE];
+	rio_t rio;
+
+	/* Read request line and headers */
+	/* rio_t 구조체에 대한 초기화 */
+	Rio_readinitb(&rio, fd); // &rio 주소를 가지는 읽기 버퍼와 식별자 connfd를 연결한다.
+	Rio_readlineb(&rio, buf, MAXLINE); // 버퍼에서 읽은 것이 담겨있다.
+	sscanf(buf, "%s %s %s", method, uri, version);
+	if (strcasecmp(method, "GET") && strcasecmp(method, "HEAD")) { //[FLAG] 11.11 숙제
+		clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
+		return;
+	}
+
+	/* GET method라면 읽어들이고, 다른 요청 헤더들을 무시하고 있다. */
+	read_requesthdrs(&rio);
+	
+	/* Parse URI form GET request */
+	/* URI 를 파일 이름과 비어 있을 수도 있는 CGI 인자 스트링으로 분석하고, 요청이 정적 또는 동적 컨텐츠를 위한 것인지 나타내는 플래그를 설정한다. */
+	is_static = parse_uri(uri, filename, cgiargs);
+
+	/* 만일 파일이 디스크상에 있지 않으면, 에러메세지를 즉시 클라아언트에게 보내고 메인 루틴으로 리턴 */
+	if (stat(filename, &sbuf) < 0) { //stat는 파일 정보를 불러오고 sbuf에 내용을 적어준다. ok 0, errer -1
+		clienterror(fd, filename, "404", "Not found", "Tiny couldn't find this file");
+		return;
+	}
+
+	if (is_static)
+	{ /* Serve static content */
+		// 파일 읽기 권한이 있는지 확인하기
+		// S_ISREG : 일반 파일인가? , S_IRUSR: 읽기 권한이 있는지? S_IXUSR 실행권한이 있는가?
+		if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
+			// 권한이 없다면 클라이언트에게 에러를 전달
+			clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
+			return;
+		}
+		// 그렇다면 클라이언트에게 파일 제공
+		serve_static(fd, filename, sbuf.st_size, method); // line:netp:doit:servestatic
+	} 
+	else 
+	{ /* Serve dynamic content */
+		/* 실행 가능한 파일인지 검증 */
+		if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) {
+			// 실행이 불가능하다면 에러를 전달
+			clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't run the CGI program");
+			return;
+		}
+		// 그렇다면 클라이언트에게 파일 제공.
+		serve_dynamic(fd, filename, cgiargs, method); // line:netp:doit:servedynamic
+	}
+}
+
+void read_requesthdrs(rio_t *rp) {
+	char buf[MAXLINE];
+
+	Rio_readlineb(rp, buf, MAXLINE);
+	
+	/* strcmp는 두 문자열을 비교하는 함수다. 헤더의 마지막 줄은 비어 있기에 \r\n 만 buf에 담겨 있다면 while문을 탈출한다. */
+	while (strcmp(buf, "\r\n"))
+	{
+		//rio_readlineb는 \n를 만날 때 멈춘다.
+		Rio_readlineb(rp, buf, MAXLINE);
+		printf("%s", buf);
+		// 멈춘 지점 까지 출력하고 다시 while.
+	}
+	return;
+}
+
+int parse_uri(char *uri, char *filename, char *cgiargs) {
+	char *ptr;
+	
+	/* strstr은 문자열 안에서 문자열로 검색하는 함수다. strstr으로 cgi-bin이 들어있는지 확인하고 양수값을 리턴하면 dynamic content를 요구하는 것이기에 조건문을 탈출. */
+	if (!strstr(uri, "cgi-bin"))
+	{ 
+		/* Static content*/
+		strcpy(cgiargs, "");
+		strcpy(filename, ".");
+		strcat(filename, uri);
+
+		//결과 cgiargs = "" 공백 문자열, filename = "./~~ or ./home.html
+		// uri 문자열 끝이 / 일 경우 home.html을 filename에 붙혀준다.
+		if (uri[strlen(uri) - 1] == '/') {
+			strcat(filename, "home.html");
+		}
+		else if (strcmp(uri, "/gif") == 0) {
+			strcpy(filename, "godzilla.gif");
+		}
+		else if (strcmp(uri, "/mp4") == 0) {
+			strcpy(filename, "moodo.mp4");
+		}
+		return 1;
+	}
+	else
+	{ 
+		/* Dynamic content*/
+		ptr = index(uri, '?');
+		// index 함수는 문자열에서 특정 문자의 위치를 반환한다
+		// CGI인자 추출
+		if (ptr) 
+		{
+			// 물음표 뒤에 있는 인자 다 갖다 붙인다.
+			// 인자로 주어진 값들을 cgiargs 변수에 넣는다.
+			strcpy(cgiargs, ptr + 1);
+			// 포인터는 문자열 마지막으로 바꾼다.
+			*ptr = '\0'; // uri물음표 뒤 다 없애기
+		}
+		else
+		strcpy(cgiargs, ""); // 물음표 뒤 인자들 전부 넣기
+		strcpy(filename, "."); // 나머지 부분 상대 uri로 바꿈,
+		strcat(filename, uri); // ./uri 가 된다.
+		return 0;
+	}
+}
+
+void serve_static(int fd, char *filename, int filesize, char *method) {
+	int srcfd;
+	char *srcp, filetype[MAXLINE], buf[MAXBUF];
+	rio_t rio;
+
+	/* Send response headers to client */
+	get_filetype(filename, filetype);    // line:netp:servestatic:getfiletype
+	sprintf(buf, "HTTP/1.0 200 OK\r\n"); // line:netp:servestatic:beginserve
+	sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
+	sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
+	sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
+	Rio_writen(fd, buf, strlen(buf)); // line:netp:servestatic:endserve
+
+	if (!strcasecmp(method, "HEAD")) {//[FLAG] 11.11 숙제 - http HEAD method
+		return;
+	}
+
+	/* Send response body to client */
+	srcfd = Open(filename, O_RDONLY, 0);                        // line:netp:servestatic:open
+	srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0); // line:netp:servestatic:mmap
+	// srcp = (char *)malloc(filesize);
+	// Rio_readn(srcfd, srcp, filesize);
+	Close(srcfd);                   // line:netp:servestatic:close
+	Rio_writen(fd, srcp, filesize); // line:netp:servestatic:write
+	// free(srcp);
+	Munmap(srcp, filesize); // line:netp:servestatic:munmap
+}
+
+void get_filetype(char *filename, char *filetype)
+{
+	if (strstr(filename, ".html"))
+		strcpy(filetype, "text/html");
+	else if (strstr(filename, ".gif"))
+		strcpy(filetype, "image/gif");
+	else if (strstr(filename, ".jpg"))
+		strcpy(filetype, "image/jpeg");
+	else if (strstr(filename, ".mp4"))
+		strcpy(filetype, "video/mp4");
+	else
+		strcpy(filetype, "text/plain");
+}
+
+void serve_dynamic(int fd, char *filename, char *cgiargs, char *method) {
+	char buf[MAXLINE], *emptylist[] = {NULL};
+
+	/* Return first part of HTTP respone */
+	sprintf(buf, "HTTP/1.0 200 OK\r\n");
+	Rio_writen(fd, buf, strlen(buf));
+	sprintf(buf, "Server: Tiny Web Server\r\n");
+	Rio_writen(fd, buf, strlen(buf));
+
+	if (!strcasecmp(method, "HEAD")) {//[FLAG] 11.11 숙제 - http HEAD method
+		return;
+	}
+
+	if (Fork() == 0) {
+		/* Child */
+		/* Real server would set all CGI vars here */
+		setenv("QUERY_STRING", cgiargs, 1);
+		Dup2(fd, STDOUT_FILENO); /* Redirect stdout to client */
+		Execve(filename, emptylist, environ); /* Run CGI program */
+	}
+	Wait(NULL); /* Parent waits for and reaps child */
+}
+
+void clienterror(int fd, char *cause, char *errnum,
+                 char *shortmsg, char *longmsg)
+{
+	char buf[MAXLINE], body[MAXBUF];
+
+	/* Build the HTTP response body */
+	sprintf(body, "<html><title>Tiny Error</title>");
+	sprintf(body, "%s<body bgcolor="
+					"ffffff"
+					">\r\n",
+			body);
+	sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+	sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+	sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+
+	/* Print the HTTP response */
+	sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+	Rio_writen(fd, buf, strlen(buf));
+	sprintf(buf, "Content-type: text/html\r\n");
+	Rio_writen(fd, buf, strlen(buf));
+	sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+	Rio_writen(fd, buf, strlen(buf));
+	Rio_writen(fd, body, strlen(body));
+}
--- proxy.c
+++ proxy.c
@@ -12,9 +12,11 @@ static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 
+void *thread(void *vargp);
+int doit(int connfd);
 int get_request(int fd, rio_t *rio, char *method, char *uri, char *version, char *headers, char *endserver);
 int request_to_server(char *method, char *uri, char *version, char *headers, char *endserver, int *clientfd);
-void send_response(int connfd, int clientfd);
+void send_response(int connfdp, int clientfd);
 void read_request(rio_t *rio, char *method, char *uri, char *version, char *headers, char *endserver);
 void make_headers(char *headers);
 void clienterror(int fd, char *cause, char *errnum,
@@ -22,12 +24,12 @@ void clienterror(int fd, char *cause, char *errnum,
 
 int main(int argc, char **argv)
 {
-  int listenfd, connfd, *clientfd;
-  char hostname[MAXLINE], port[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE], headers[MAXLINE], endserver[MAXLINE];
+  int listenfd;
+  int *connfdp;
+  // char hostname[MAXLINE], port[MAXLINE];
   socklen_t clientlen;
   struct sockaddr_storage clientaddr;
-  rio_t rio;
-  strcpy(uri, "");
+  pthread_t tid;
 
   if (argc != 2)
   {
@@ -35,28 +37,52 @@ int main(int argc, char **argv)
     exit(1);
   }
 
-  listenfd = Open_listenfd(argv[1]);
+  listenfd = Open_listenfd(argv[1]); // 전달 받은 포트 번호를 사용해 listen 소켓 생성.
+
   while (1)
   {
     clientlen = sizeof(clientaddr);
-    connfd = Accept(listenfd, (SA *)&clientaddr,
-                    &clientlen);
-    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE,
-                0);
-    printf("Accepted connection from host: (%s, %s)\n", hostname, port);
 
-    Rio_readinitb(&rio, connfd);
+    connfdp = Malloc(sizeof(int));
+    *connfdp = Accept(listenfd, (SA *)&clientaddr, &clientlen); // 클라이언트 연결 요청 수신
 
-    if (get_request(connfd, &rio, method, uri, version, headers, endserver) < 0) // 굳이 version을 받아올 의미가 있나? 어차피 다 1.0으로 보낼건데?
-    {
-      Close(connfd);
-    };
-    make_headers(headers);
-    request_to_server(method, uri, version, headers, endserver, &clientfd); // 응답 못 받았을 때의 처리 필요?
-    send_response(connfd, clientfd);
-    Close(connfd);
+    // Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+    // printf("Accepted connection from host: (%s, %s)\n", hostname, port);
+
+    Pthread_create(&tid, NULL, thread, connfdp);
   }
   return 0;
+}
+
+int doit(int connfdp)
+{
+    int *clientfd;
+    rio_t rio;
+    char method[MAXLINE], uri[MAXLINE], version[MAXLINE], headers[MAXLINE], endserver[MAXLINE];
+    strcpy(uri, ""); // 초기화!
+
+    Rio_readinitb(&rio, connfdp); // 구조체 rio에 connfd을 할당하겠다는 초기화.
+
+    if (get_request(connfdp, &rio, method, uri, version, headers, endserver) < 0) // 굳이 version을 받아올 의미가 있나? 어차피 다 1.0으로 보낼건데?
+    {// 에러 처리. get_request()에서 -1이 리턴된 경우 connfd를 최종적으로 닫아준 뒤 모든 수행을 종료한다.
+      Close(connfdp);
+    };
+
+    make_headers(headers);
+    request_to_server(method, uri, version, headers, endserver, &clientfd); // 응답 못 받았을 때의 처리 필요?
+    send_response(connfdp, clientfd);
+
+    return 0;
+}
+
+void *thread(void *vargp) // 
+{
+    int connfd = *((int *)vargp); // void pointer는 주소값만 담겨 있고 연산이나 참조를 할 수 없기 때문에 int pointer로 우선 캐스팅했다.
+    Pthread_detach(pthread_self());
+    Free(vargp);
+    doit(connfd);
+    Close(connfd);
+    return NULL;
 }
 
 int get_request(int fd, rio_t *rio, char *method, char *uri, char *version, char *headers, char *endserver)
@@ -69,6 +95,7 @@ int get_request(int fd, rio_t *rio, char *method, char *uri, char *version, char
                 "Tiny does not implement this method");
     return -1;
   }
+
   if (!strcmp(version, "HTTP/1.1"))
   {
     strcpy(version, "HTTP/1.0");
@@ -88,12 +115,15 @@ void read_request(rio_t *rio, char *method, char *uri, char *version, char *head
   int is_request_line;
   char *host_idx, *host_name_s, *path;
   is_request_line = 1;
+  // 초기화!
   strcpy(uri, "");
   strcpy(buf, "");
+
   while (strcmp(buf, "\r\n"))
   {
     if (is_request_line)
     {
+      // if문 안쪽은 method, uri, version에 대한 정보가 담겨 있는 첫번째 줄을 읽어오는 수행을 한다.
       Rio_readlineb(rio, buf, MAXLINE);
       sscanf(buf, "%s %s %s", method, uri, version);
       host_name_s = strstr(uri, "http://");
@@ -109,7 +139,7 @@ void read_request(rio_t *rio, char *method, char *uri, char *version, char *head
       is_request_line = 0;
     }
     else
-    {
+    { // 나머지 요청의 내용을 읽는다. 헤더 정보 중 변경해줘야 하는 설정들이 있다면 수정해준다.
       Rio_readlineb(rio, buf, MAXLINE);
       if (strstr(buf, "Connection:"))
       {
@@ -131,25 +161,27 @@ void read_request(rio_t *rio, char *method, char *uri, char *version, char *head
 
 void make_headers(char *headers)
 {
-  char new_header[MAXLINE];
-  strcpy(new_header, "");
-  strncpy(new_header, headers, strlen(headers) - 2);
+  char tmp[MAXLINE];
+  strcpy(tmp, ""); // 빈 변수 tmp를 선언한다.
+  strncpy(tmp, headers, strlen(headers) - 2); // read_request()에서 받아온 headers의 내용 전체에서 맨 마지막의 \r\n을 제외하여 빈 변수 tmp에 담는다.
 
+  // 필수 헤더 정보 미포함 시 내용을 추가해준다.
   if (!strstr(headers, "User-Agent:"))
   {
-    strcat(new_header, user_agent_hdr);
+    strcat(tmp, user_agent_hdr);
   }
   if (!strstr(headers, "Connection:"))
   {
-    strcat(new_header, "Connection: close\n");
+    strcat(tmp, "Connection: close\n");
   }
   if (!strstr(headers, "Proxy-Connection:"))
   {
-    strcat(new_header, "Proxy-Connection: close\n");
+    strcat(tmp, "Proxy-Connection: close\n");
   }
-  strcat(new_header, "\r\n");
-  strcpy(headers, "");
-  strcpy(headers, new_header);
+
+  strcat(tmp, "\r\n"); // tmp의 끝에 \r\n을 붙여준다.
+  strcpy(headers, ""); // free().
+  strcpy(headers, tmp); // 변수 headers에 갱신된 헤더의 내용을 도로 담는다.
 }
 
 int request_to_server(char *method, char *uri, char *version, char *headers, char *endserver, int *clientfd)
@@ -157,14 +189,15 @@ int request_to_server(char *method, char *uri, char *version, char *headers, cha
   char *is_port, *rest_uri;
   char request_uri[MAXLINE], full_http_request[MAXLINE], request_port[MAXLINE];
 
-  is_port = strstr(endserver, ":");
+  is_port = strstr(endserver, ":"); // 요청의 내용에 서버의 포트 번호가 있었다면 is_port == 0. 포트 번호에 대한 정보의 유무를 콜론의 유무로 확인하고 있다.
+
   if (!is_port)
-  {
+  {// NULL이면, 즉 요청값 중 포트번호에 대한 정보가 없었다면.
     strcpy(request_uri, endserver);
     strcpy(request_port, "80");
   }
   else
-  {
+  {// NULL이 아니면, 즉 요청값 중 포트번호에 대한 정보가 있었다면.
     strncpy(request_uri, endserver, (int)(is_port - endserver));
     strcpy(request_port, is_port + 1);
   }
@@ -184,15 +217,18 @@ int request_to_server(char *method, char *uri, char *version, char *headers, cha
   return 0;
 };
 
-void send_response(int connfd, int clientfd)
+void send_response(int connfdp, int clientfd)
 {
   char buf[MAXLINE], header[MAXLINE];
   rio_t rio;
   int content_length;
 
-  Rio_readinitb(&rio, clientfd);
+  Rio_readinitb(&rio, clientfd); // 구조체 rio에 clientfd을 할당하겠다는 초기화.
+
+  // 초기화!
   strcpy(buf, "");
   strcpy(header, "");
+
   while (strcmp(buf, "\r\n"))
   {
     Rio_readlineb(&rio, buf, MAXLINE);
@@ -201,18 +237,21 @@ void send_response(int connfd, int clientfd)
       char *p = strchr(buf, ':');
       char temp_str[MAXLINE];
       strcpy(temp_str, p + 1);
-      content_length = atoi(temp_str);
+      content_length = atoi(temp_str); // header안에 들어 있는 Content-length 정보를 따로 변수 content_length에 담아 추출한다.
     }
     strcat(header, buf);
   }
-  Rio_writen(connfd, header, strlen(header));
+
+  Rio_writen(connfdp, header, strlen(header));
 
   char *body = malloc(content_length);
+
   Rio_readnb(&rio, body, content_length);
-  Rio_writen(connfd, body, content_length);
+  Rio_writen(connfdp, body, content_length);
 
   free(body);
-  Close(clientfd);
+  
+  // Close(clientfd);
 }
 
 void clienterror(int fd, char *cause, char *errnum,
