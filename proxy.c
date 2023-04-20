0cdcac1beee780df8c49b98ef242c4758a8f4ee5
Ubuntu <ubuntu@ip-172-31-34-183.ap-northeast-2.compute.internal>
2023-04-20 01:22:01 +0000

    Part III: Caching web objects

--- proxy.c
+++ proxy.c
@@ -12,13 +12,29 @@ static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 
-void *thread(void *vargp);
-int doit(int connfd);
+struct cache {
+  char uri[MAXLINE];
+  char object[MAX_OBJECT_SIZE];
+  int size;
+  struct cache *prev;
+  struct cache *next;
+  time_t timestamp;
+};
+
+struct cache *cache_list = NULL;
+int cache_size = 0;
+int is_hit = 0;
+
+struct cache *cache_lookup(char *uri);
+void cache_insert(char *uri, char *object, int size);
+void cache_remove();
 int get_request(int fd, rio_t *rio, char *method, char *uri, char *version, char *headers, char *endserver);
-int request_to_server(char *method, char *uri, char *version, char *headers, char *endserver, int *clientfd);
-void send_response(int connfdp, int clientfd);
-void read_request(rio_t *rio, char *method, char *uri, char *version, char *headers, char *endserver);
+void read_request(rio_t *rio, char *method, char *uri, char *version, char *headers, char *endserver, int fd);
 void make_headers(char *headers);
+int request_to_server(char *method, char *uri, char *version, char *headers, char *endserver, int *clientfd);
+void send_response(int connfdp, int clientfd, char *uri);
+int doit(int connfd);
+void *thread(void *vargp);
 void clienterror(int fd, char *cause, char *errnum,
                  char *shortmsg, char *longmsg);
 
@@ -26,7 +42,6 @@ int main(int argc, char **argv)
 {
   int listenfd;
   int *connfdp;
-  // char hostname[MAXLINE], port[MAXLINE];
   socklen_t clientlen;
   struct sockaddr_storage clientaddr;
   pthread_t tid;
@@ -44,13 +59,12 @@ int main(int argc, char **argv)
     clientlen = sizeof(clientaddr);
 
     connfdp = Malloc(sizeof(int));
-    *connfdp = Accept(listenfd, (SA *)&clientaddr, &clientlen); // 클라이언트 연결 요청 수신
-
-    // Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
-    // printf("Accepted connection from host: (%s, %s)\n", hostname, port);
+    *connfdp = Accept(listenfd, (SA *)&clientaddr, &clientlen); // 클라이언트 연결 요청 수신.
 
     Pthread_create(&tid, NULL, thread, connfdp);
+    printf("Now Pthread_create returns while main\n");
   }
+  printf("%s\n", user_agent_hdr);
   return 0;
 }
 
@@ -66,28 +80,32 @@ int doit(int connfdp)
     if (get_request(connfdp, &rio, method, uri, version, headers, endserver) < 0) // 굳이 version을 받아올 의미가 있나? 어차피 다 1.0으로 보낼건데?
     {// 에러 처리. get_request()에서 -1이 리턴된 경우 connfd를 최종적으로 닫아준 뒤 모든 수행을 종료한다.
       Close(connfdp);
-    };
-
-    make_headers(headers);
+    }
+    if (is_hit)
+    {// 캐시 히트라면 return~!
+      printf("Function 'doit' returns by cache hit\n");
+      return 0;
+    }
     request_to_server(method, uri, version, headers, endserver, &clientfd); // 응답 못 받았을 때의 처리 필요?
-    send_response(connfdp, clientfd);
+    send_response(connfdp, clientfd, uri);
 
     return 0;
 }
 
-void *thread(void *vargp) // 
+void *thread(void *vargp)
 {
     int connfd = *((int *)vargp); // void pointer는 주소값만 담겨 있고 연산이나 참조를 할 수 없기 때문에 int pointer로 우선 캐스팅했다.
     Pthread_detach(pthread_self());
     Free(vargp);
     doit(connfd);
     Close(connfd);
+    printf("Now connfd is closed while thread\n");
     return NULL;
 }
 
 int get_request(int fd, rio_t *rio, char *method, char *uri, char *version, char *headers, char *endserver)
 {
-  read_request(rio, method, uri, version, headers, endserver);
+  read_request(rio, method, uri, version, headers, endserver, fd);
 
   if (strcasecmp(method, "GET") && strcasecmp(method, "HEAD")) // RFC 1945 - GET/HEAD/POST + 토큰의 extension-method..?
   {
@@ -96,20 +114,19 @@ int get_request(int fd, rio_t *rio, char *method, char *uri, char *version, char
     return -1;
   }
 
-  if (!strcmp(version, "HTTP/1.1"))
-  {
-    strcpy(version, "HTTP/1.0");
-  }
+  make_headers(headers);
+
   if (!strstr(headers, "Host:"))
   {
     clienterror(fd, headers, "400", "Bad Request",
                 "Host header is empty in the request");
     return -1;
   }
+
   return 0;
 }
 
-void read_request(rio_t *rio, char *method, char *uri, char *version, char *headers, char *endserver)
+void read_request(rio_t *rio, char *method, char *uri, char *version, char *headers, char *endserver, int fd)
 {
   char buf[MAXLINE];
   int is_request_line;
@@ -136,6 +153,20 @@ void read_request(rio_t *rio, char *method, char *uri, char *version, char *head
       {
         strcpy(uri, "/");
       }
+
+      // 캐시 체크하기 - 있다면 캐시 히트~!
+      struct cache *cached_item = cache_lookup(uri);
+
+      if (cached_item != NULL) {
+        is_hit = 1;
+        printf("===CACHE HIT %d===\n", is_hit);
+        printf("Proxy read %d bytes from cache and sent to client\n", cached_item->size);
+        Rio_writen(fd, cached_item->object, cached_item->size);
+        printf("object: %s\n", cached_item->object);
+
+        return;
+      }
+
       is_request_line = 0;
     }
     else
@@ -156,6 +187,11 @@ void read_request(rio_t *rio, char *method, char *uri, char *version, char *head
       }
       strcat(headers, buf);
     }
+  }
+
+  if (!strcmp(version, "HTTP/1.1"))
+  {
+    strcpy(version, "HTTP/1.0");
   }
 }
 
@@ -217,11 +253,14 @@ int request_to_server(char *method, char *uri, char *version, char *headers, cha
   return 0;
 };
 
-void send_response(int connfdp, int clientfd)
+void send_response(int connfdp, int clientfd, char *uri)
 {
   char buf[MAXLINE], header[MAXLINE];
   rio_t rio;
   int content_length;
+  char cache_buf[MAX_OBJECT_SIZE];
+  int cache_buf_size = 0;
+  size_t n;
 
   Rio_readinitb(&rio, clientfd); // 구조체 rio에 clientfd을 할당하겠다는 초기화.
 
@@ -231,7 +270,7 @@ void send_response(int connfdp, int clientfd)
 
   while (strcmp(buf, "\r\n"))
   {
-    Rio_readlineb(&rio, buf, MAXLINE);
+    n = Rio_readlineb(&rio, buf, MAXLINE);
     if (strstr(buf, "Content-length:"))
     {
       char *p = strchr(buf, ':');
@@ -240,6 +279,13 @@ void send_response(int connfdp, int clientfd)
       content_length = atoi(temp_str); // header안에 들어 있는 Content-length 정보를 따로 변수 content_length에 담아 추출한다.
     }
     strcat(header, buf);
+
+    cache_buf_size += n;
+    printf("===CACHE BUF SIZE header %d while send_response===\n", n);
+
+    if (cache_buf_size < MAX_OBJECT_SIZE) {
+      strcat(cache_buf, buf);
+    }
   }
 
   Rio_writen(connfdp, header, strlen(header));
@@ -249,9 +295,92 @@ void send_response(int connfdp, int clientfd)
   Rio_readnb(&rio, body, content_length);
   Rio_writen(connfdp, body, content_length);
 
+  cache_buf_size += content_length;
+  printf("===CACHE BUF SIZE body %d while send_response===\n", n);
+
+  if (cache_buf_size < MAX_OBJECT_SIZE) {
+    strcat(cache_buf, body);
+  }
+
   free(body);
+
+  if (cache_size < MAX_OBJECT_SIZE) {
+    cache_insert(uri, cache_buf, cache_buf_size);
+  }
   
-  // Close(clientfd);
+  Close(clientfd);
+}
+
+struct cache *cache_lookup(char *uri) {
+  printf("===cache_lookup while get_request %s===\n", uri);
+  struct cache *ptr = cache_list;
+  while (ptr != NULL) {
+    if (strcmp(ptr->uri, uri) == 0) {
+      // 현재 시간으로 업데이트.
+      ptr->timestamp = time(NULL);
+      return ptr;
+    }
+    ptr = ptr->next;
+  }
+  return NULL;
+}
+
+void cache_insert(char *uri, char *object, int size) {
+  struct cache *new_cache = malloc(sizeof(struct cache));
+  strcpy(new_cache->uri, uri);
+  strcpy(new_cache->object, object);
+  new_cache->size = size;
+  // list의 맨 앞에 삽입한다.
+  new_cache->prev = NULL;
+  new_cache->next = cache_list;
+  if (cache_list != NULL) {
+    cache_list->prev = new_cache;
+  }
+  cache_list = new_cache;
+  printf("==while cache_insert of send_response cache_size bf: %d==\n", cache_size);
+  cache_size = cache_size + size;
+  printf("==while cache_insert of send_response cache_size atf: %d==\n", cache_size);
+
+  new_cache->timestamp = time(NULL);
+
+  while(cache_size > MAX_CACHE_SIZE) {
+    cache_remove();
+  }
+}
+
+// 캐시에서 쫓아낼 친구를 찾는다. LRU 알고리즘이 활용된다.
+void cache_remove() {
+  struct cache *ptr = cache_list;
+  time_t oldest_time = time(NULL);
+  struct cache *oldest = NULL;
+
+  while (ptr != NULL) {
+    if (ptr->timestamp < oldest_time) {
+      oldest = ptr;
+      oldest_time = oldest->timestamp;
+    }
+    ptr = ptr->next;
+  }
+
+  if (oldest != NULL) {
+    // 맨 앞 삭제
+    if (oldest->prev == NULL) {
+      oldest->next->prev = NULL;
+      cache_list = oldest->next;
+    }
+    // 맨 뒤 삭제
+    else if (oldest->next == NULL) {
+      oldest->prev->next = NULL;
+      oldest->prev = NULL;
+    }
+    // 중간 부분 삭제 
+    else {
+      oldest->prev->next = oldest->next;
+      oldest->next->prev = oldest->prev;
+    }
+    cache_size -= oldest->size;
+    free(oldest);
+  }
 }
 
 void clienterror(int fd, char *cause, char *errnum,
